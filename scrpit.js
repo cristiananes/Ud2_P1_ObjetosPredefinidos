a26094e7bf9df232b5fc0aa14abf881504ee282a
cristian <cristian-anes@hotmail.es>
2023-10-17 22:31:14 +0200

    Comiendome la cabeza con el primer ejercicio, voy a cenar

---
+++ scrpit.js
@@ -0,0 +1,6 @@
+//El enunciado nos pide una función que de la vuelta a un  texto 
+//Después nos piden otra función que lea el texto por caracteres y sustituya el caracter por un separador de texto 
+function separarPalabras(texto, separador) {
+    return texto.split(separador);
+    }
+    
